5743f244c384091cf1871b98210abb82a29de637
hanh <haid3036@gmail.com>
2023-06-12 15:05:56 +0700

    a

--- a.py
+++ a.py
@@ -8,7 +8,7 @@
 # import a,ab,ac,ad,ae,af,ag,ah,aj,ak,al,am,an,ao,ap,ar,mk,at,au,av,aw,ay,az,b,ba,bc,bd,be,bf,bg,bh,bi,bj,bk,bl,bm
 # import bn,bo,bq,bp,br,bs,bt,bv,bu,bư,bx,by,bz,c,ca,cb,cd,ce,cf,cg,ch,ci,cj,ck,cl,cm,cn,cq,cr,cr,cs,ct,cu,cư,cv,cx,cy
 # import cz,d,de,dq,dr,e,f,g,h,i,j,k,l,m,n,o,q,r,s,t,u,ư,v,w,x,y,z,kj
-from home.runner.work.telepot6.telepot6.cv2.python-310 import cv2
+from home.runner.work.telepot6.telepot6.cv2.python-3.10 import cv2
 # from telepot7.vc16 import cv2
 import telepot
 import numpy
