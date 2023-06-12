6acfcdeaddaf37ca9bcd0b7e56a88e95614db8b5
hanh <haid3036@gmail.com>
2023-06-12 10:20:50 +0700

    a

--- a.py
+++ a.py
@@ -11,7 +11,7 @@
 import cv2
 import telepot
 import numpy
-from /home/runner/work/telepot3/telepot3/ultralytics import YOLO
+from .home.runner.work.telepot3.telepot3.ultralytics import YOLO
 model = YOLO("yolov8n.pt")
 results = model("rtsp://admin:admin1234@192.168.1.15:554/cam/realmonitor?channel=1&subtype=0",stream=True,show=True)
 def show_frame():
